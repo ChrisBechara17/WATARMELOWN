c911c216a111f3ddbb3adb4cbaf7bfe5b83b4b38
ChrisBechara17 <chrisbechara91@gmail.com>
2025-10-18 10:52:53 +0300

    Configure SQLite path to App_Data and apply migrations at startup; add out/ to .gitignore

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 using Microsoft.EntityFrameworkCore;
+using SimpleMarketApp.Data;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<SimpleMarketApp.Data.AppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Resolve SQLite database path to a writable location and register DbContext
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var appDataDirectory = Path.Combine(builder.Environment.ContentRootPath, "App_Data");
+Directory.CreateDirectory(appDataDirectory);
+var databaseFilePath = Path.Combine(appDataDirectory, "MarketDB.db");
+connectionString = $"Data Source={databaseFilePath}";
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(connectionString));
     
 var app = builder.Build();
 
@@ -30,5 +41,11 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+// Apply EF Core migrations on startup to ensure database and tables exist
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
 
 app.Run();
